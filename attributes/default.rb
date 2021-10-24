135eb22139787cdeb87ff2c7c9d6fbcc7c965352
Jim Dowling <jim@logicalclocks.com>
2021-10-24 17:41:35 +0200

    fix python version

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,7 @@ include_attribute "hops"
 # User configuration
 default['airflow']["airflow_package"] = 'apache-airflow' 
 default['airflow']["version"]         = "1.10.10"
-default['airflow']['url']             = "#{node['download_url']}/apache/airflow/#{node['airflow']['version']}/constraints/constraints-3.7.txt"
+default['airflow']['url']             = "#{node['download_url']}/apache/airflow/#{node['airflow']['version']}/constraints/constraints-#{node['install']['python']['version']}.txt"
 default['airflow']['user']            = node['install']['user'].empty? ? 'airflow' : node['install']['user']
 default['airflow']['user_id']         = '1512'
 default['airflow']['group']           = node['install']['user'].empty? ? 'airflow' : node['install']['user']
