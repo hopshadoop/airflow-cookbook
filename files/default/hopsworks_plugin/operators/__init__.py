3d4968f12d429994d4c3870f586f4ab17be3d4ca
Fabio Buso <buso.fabio@gmail.com>
2021-10-27 00:22:02 +0200

    Remove broken feature validation operator

--- files/default/hopsworks_plugin/operators/__init__.py
+++ files/default/hopsworks_plugin/operators/__init__.py
@@ -1,9 +1,7 @@
 from hopsworks_plugin.operators.hopsworks_operator import HopsworksLaunchOperator
 from hopsworks_plugin.operators.hopsworks_operator import HopsworksModelServingInstance
-from hopsworks_plugin.operators.hopsworks_operator import HopsworksFeatureValidationResult
 
 HOPSWORKS_OPERATORS = [
     HopsworksLaunchOperator,
-    HopsworksModelServingInstance,
-    HopsworksFeatureValidationResult    
+    HopsworksModelServingInstance
 ]
