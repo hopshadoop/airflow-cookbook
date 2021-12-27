3b563f1e6b7803b88fc2d2fbbbe5cecb8052f562
Fabio Buso <buso.fabio@gmail.com>
2021-12-27 18:03:47 +0100

    Move user creation in the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include_recipe "hops_airflow::user"
+include_recipe "hops_airflow::directories"
 include_recipe "hops_airflow::image"
 include_recipe "hops_airflow::db"
 include_recipe "hops_airflow::config"
