6af0972ee9a4e2bf54cdafbb695f26f71bcd142b
Antonios Kouzoupis <antonios@logicalclocks.com>
2022-03-15 11:45:44 +0100

    [HOPSWORKS-2578] Delete old secrets and logs directory even if it is empty

--- recipes/directories.rb
+++ recipes/directories.rb
@@ -96,11 +96,18 @@ bash 'Move airflow logs to data volume' do
   code <<-EOH
     set -e
     mv -f #{node["airflow"]["log_link"]}/* #{node['airflow']['data_volume']['log_dir']}
-    rm -rf #{node["airflow"]["log_link"]}
   EOH
   only_if { conda_helpers.is_upgrade }
-  only_if { File.directory?(node["airflow"]["log_link"])}
-  not_if { File.symlink?(node["airflow"]["log_link"])}
+  only_if { File.directory?(node["airflow"]["log_link"]) }
+  not_if { File.symlink?(node["airflow"]["log_link"]) }
+  not_if { Dir.empty?(node['airflow']['log_link']) }
+end
+
+directory node['airflow']['log_link'] do
+  action :delete
+  only_if { conda_helpers.is_upgrade }
+  only_if { File.directory?(node['airflow']['log_link']) }
+  not_if { File.symlink?(node['airflow']['log_link']) }
 end
 
 link node['airflow']['log_link'] do
@@ -125,12 +132,18 @@ bash 'Move airflow secrets to data volume' do
   code <<-EOH
     set -e
     mv -f #{node['airflow']['secrets_link']}/* #{node['airflow']['data_volume']['secrets_dir']}
-    rm -rf #{node['airflow']['secrets_link']}
   EOH
   only_if { conda_helpers.is_upgrade }
   only_if { File.directory?(node['airflow']['secrets_link'])}
   not_if { File.symlink?(node['airflow']['secrets_link'])}
   not_if { Dir.empty?(node['airflow']['secrets_link']) }
+end
+
+directory node['airflow']['secrets_link'] do
+  action :delete
+  only_if { conda_helpers.is_upgrade }
+  only_if { File.directory?(node['airflow']['secrets_link']) }
+  not_if { File.symlink?(node['airflow']['secrets_link']) }
 end
 
 link node['airflow']['secrets_link'] do
