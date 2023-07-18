25cb40f19753a174075115ed672cd2e56c9f9df5
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-18 14:20:21 +0200

    Move airflow dags to hopsfs

--- recipes/scheduler.rb
+++ recipes/scheduler.rb
@@ -26,6 +26,15 @@ else
   service_template = "airflow-scheduler.service.erb"
 end
 
+namenode_fdqn = consul_helper.get_service_fqdn("rpc.namenode")
+apparmor_security_opt=""
+apparmor_enabled = is_apparmor_enabled()
+if apparmor_enabled
+  apparmor_security_opt="--security-opt apparmor:#{node['hops']['docker']['hopsfsmount-apparmor-profile']}"
+end
+
+docker_fuse_args = "--device /dev/fuse #{apparmor_security_opt} --security-opt seccomp=#{node['hops']['docker']['hopsfsmount-seccomp-profile']}"
+
 image_name = "#{consul_helper.get_service_fqdn("registry")}:#{node['hops']['docker']['registry']['port']}/airflow:#{node['airflow']['version']}"
 
 template service_target do
@@ -36,6 +45,8 @@ template service_target do
   variables({
     :deps => deps,
     :image_name => image_name,
+    :docker_fuse_args => docker_fuse_args,
+    :namenode_fdqn => namenode_fdqn
   })
 end
 
