bd99136f32ec8344b7a6b94dfc962ff3d472a6ea
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-24 09:51:39 +0200

    Include hops helpers

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+Chef::Recipe.send(:include, Hops::Helpers)
+
 include_recipe "hops_airflow::image"
 include_recipe "hops_airflow::config"
 include_recipe "hops_airflow::db"
