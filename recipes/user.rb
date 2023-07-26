3ecb664879d7e622d1faecf4de84aca30d8bff2d
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-26 20:24:12 +0200

    fix

--- recipes/user.rb
+++ recipes/user.rb
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+Chef::Recipe.send(:include, Hops::Helpers)
 
 group node['airflow']['group'] do
   gid node['airflow']['group_id']
