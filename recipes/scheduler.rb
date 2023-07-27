759752e2a880accc21578ddd7e369ef679f1a54e
Gibson Chikafa <gibbschikafa@gmail.com>
2023-07-27 05:03:54 +0200

    Include the helpers

--- recipes/scheduler.rb
+++ recipes/scheduler.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+Chef::Recipe.send(:include, Hops::Helpers)
 
 deps = ""
 if exists_local("ndb", "mysqld") 
--- recipes/webserver.rb
+++ recipes/webserver.rb
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+Chef::Recipe.send(:include, Hops::Helpers)
 
 deps = ""
 if exists_local("ndb", "mysqld") 
